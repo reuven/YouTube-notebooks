03b4c90d7da00e79d69026788110e4ad7db0b014
Reuven Lerner <reuven@lerner.co.il>
2021-04-01 18:48:45 +0300

    from import *

---
+++ mymod.py
@@ -0,0 +1,13 @@
+__all__ = ['YOU_SHOULD_NEVER_USE_IMPORT_STAR']
+
+_secret = 'shhh!'
+
+x = 100
+
+y = [10, 20, 30]
+
+z = {'a': 1, 'b': 2, 'c': 3}
+
+
+def hello(name):
+    return f'Hello, {name}!'
